80baea67abe4c81f2bde0e6488d9807a6491d7e1
Jeroen Evens <jeroen7e@gmail.com>
2018-12-14 15:10:42 +0100

    [MIG] $MODULE: Migration to 12.0

--- account_cutoff_accrual_base/__manifest__.py
+++ account_cutoff_accrual_base/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Account Accrual Base',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Base module for accrued expenses and revenues',
